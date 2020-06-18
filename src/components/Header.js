113fc4c6dfbb48fc405b183cbc933cd25f3a0b6d
aamirali <sufaamir@gmail.com>
2020-06-18 11:10:12 -0400

    update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,9 @@
-import React from 'react';
+import React from 'react'
 
 export const Header = () => {
-	return <h2>Expense Tracker Bootcamp 1st Assigment Student Name: Aamir Ali</h2>;
+    return (
+        <h2>
+            Expense Tracker Bootcamp 1st Assigment Student Name: Aamir Ali
+        </h2>
+    );
 };
